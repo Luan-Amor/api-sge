e9cf9927e1c58727df3b647f1553ffd46ae153e6
Luan-Amor <luansamor@hotmail.com>
2022-08-21 14:16:24 -0300

    Add script start

--- src/controllers/AuthenticationController.ts
+++ src/controllers/AuthenticationController.ts
@@ -15,11 +15,4 @@ export class AuthenticationController {
         res.json(result);
     }
 
-    async logout(req, res, next){
-        const authToken = req.headers["authorization"];
-        const token = authToken.split(' ')[1];
-        const service = new AutenticationService();
-        await service.logout(token);
-        res.send();
-    }
 }
